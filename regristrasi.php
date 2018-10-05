f4706bfe0e6fb395c66720c014769eca41d11400
GrimadhaPratamaAji <43518826+GrimadhaPratamaAji@users.noreply.github.com>
2018-10-05 10:22:24 +0700

    Update regristrasi.php

--- regristrasi.php
+++ regristrasi.php
@@ -1,11 +1,23 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Form Registrasi</title>
-</head>
-<body>
-
-
-
-</body>
-</html>
+<form method="POST">
+ <table>
+  <tr>
+   <td>NIM</td>
+   <td>:</td>
+   <td><input type="text" name="nim"></td>
+  </tr>
+  <tr>
+   <td>NAMA</td>
+   <td>:</td>
+   <td><input type="text" name="nama"></td>
+  </tr>
+   <tr>
+   <td>EMAIL</td>
+   <td>:</td>
+   <td><input type="text">@gmail.com<name="email"></td>
+  </tr>
+  <tr>
+   <td></td>
+   <td><input type="submit" name="submit" value="REGISTRASI"></td>
+  </tr>
+ </table>
+</form>
