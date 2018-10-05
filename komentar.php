6a53078d76be6870691f982ce3300f47949f5b00
GrimadhaPratamaAji <43518826+GrimadhaPratamaAji@users.noreply.github.com>
2018-10-05 10:34:26 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,3 +1,24 @@
-<?php
+<form action="" method="POST">
+  <table>
+    <tr>
+      <td>NIM</td>
+      <td>:</td>
+      <td><textarea name="komentar"></textarea>
+      </td>
 
-?>
+      <td></td>
+      <td><input type="submit" name="kirim" value="kirim"></td>
+
+    </tr>
+  </table>
+
+  <?php
+  if (isset($_POST['kirim'])) {
+    $komentar = $_POST['komentar'];
+
+    if(str_word_count($komentar)<5){
+      echo"masukan angka dengan maksimal 10 digit";
+    }else{
+      echo str_word_count($komentar);
+    }
+  } ?>
